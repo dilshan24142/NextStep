f7bf8cdc9f62891b2bfe97c0047e43d8dd5e835a
chamodyha123 <kcpkehelgamuwa@students.nsbm.ac.lk>
2026-02-11 12:26:17 +0530

    fixing bugs

--- src/main/java/com/securitygateway/nextstep/configuration/WebConfig.java
+++ src/main/java/com/securitygateway/nextstep/configuration/WebConfig.java
@@ -1,3 +1,5 @@
+package com.securitygateway.nextstep.configuration;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
